2190e6314b7d180c63f468dae9c1c62fa303db12
BIT <BIT@DESKTOP-F3NN5PP>
2018-12-26 17:58:15 +0900

    a

--- src/practice01/Prob1.java
+++ src/practice01/Prob1.java
@@ -5,7 +5,11 @@ import java.util.Scanner;
 public class Prob1 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner( System.in );
+		int input = scanner.nextInt();
 		
+		if(input % 3 == 0) {
+			System.out.println("3의 배수입니다.");
+		}
 		
 		scanner.close();
 	}
